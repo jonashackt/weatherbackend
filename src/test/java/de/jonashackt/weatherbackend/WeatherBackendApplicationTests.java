ebba58904dc6e04d5246ece6ef16ddc925290b7a
jonashackt <jonas.hecht@codecentric.de>
2016-09-05 09:57:47 +0200

    working example of REST library http://unirest.io/java.html

--- src/test/java/de/jonashackt/weatherbackend/WeatherBackendApplicationTests.java
+++ src/test/java/de/jonashackt/weatherbackend/WeatherBackendApplicationTests.java
@@ -3,6 +3,12 @@ package de.jonashackt.weatherbackend;
 import static com.jayway.restassured.RestAssured.*;
 import static org.junit.Assert.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
 import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +23,8 @@ import de.codecentric.soap.businesslogic.IncredibleLogic;
 import de.codecentric.soap.internalmodel.GeneralOutlook;
 import de.codecentric.soap.internalmodel.Product;
 import de.codecentric.soap.internalmodel.Weather;
+
+import java.io.IOException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = WeatherBackendApplication.class)
@@ -50,10 +58,44 @@ public class WeatherBackendApplicationTests {
 	    
 	    assertEquals("Weimar", outlook.getCity());
     }
-	
-	@Before
-	public void setUp() {
-	    
-	}
 
+	@Test
+	public void testWithUniRest() throws Exception {
+
+        Unirest.setObjectMapper(new ObjectMapper() {
+            private com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper
+                    = new com.fasterxml.jackson.databind.ObjectMapper();
+
+            public <T> T readValue(String value, Class<T> valueType) {
+                try {
+                    jacksonObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                    return jacksonObjectMapper.readValue(value, valueType);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public String writeValue(Object value) {
+                try {
+                    jacksonObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                    return jacksonObjectMapper.writeValueAsString(value);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Weather weather = new Weather();
+        weather.setFlagColor("blue");
+        weather.setPostalCode("99425");
+        weather.setProduct(Product.ForecastBasic);
+
+        HttpResponse<GeneralOutlook> generalOutlookHttpResponse = Unirest.post("http://localhost:8080/weatherbackend/general/outlook")
+                .header("accept", "application/json")
+                .header("Content-Type", "application/json")
+                .body(weather)
+                .asObject(GeneralOutlook.class);
+
+        assertEquals("Weimar", generalOutlookHttpResponse.getBody().getCity());
+	}
 }
